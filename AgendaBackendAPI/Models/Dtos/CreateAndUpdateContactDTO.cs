5db98dc20ba5493b7415659e2b60bf5e50a167e0
brendaahumada <bahumada@mail.austral.edu.ar>
2023-11-30 20:06:53 -0300

    cambios 30112023

--- AgendaBackendAPI/Models/Dtos/CreateAndUpdateContactDTO.cs
+++ AgendaBackendAPI/Models/Dtos/CreateAndUpdateContactDTO.cs
@@ -7,7 +7,6 @@ namespace AgendaBackendAPI.Models.Dtos
         [Required]
         public string name { get; set; }
         public string? lastName { get; set; }
-        public string? avatar { get; set; }
         public string? description { get; set; }
         public string? email { get; set; }
         public long? celularNumber { get; set; }
